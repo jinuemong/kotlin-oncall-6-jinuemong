7ed9c340426b6b80aaaa59b2940478729ca7f643
jinuemong <jinwoo3661@naver.com>
2023-12-16 17:31:12 +0900

    [Test]: 휴일 테스트 기능 구현
    (cherry picked from commit 12612dc0bd13453dce80dedfc202329028e12393)

---
+++ src/test/kotlin/oncall/domain/HolidayTest.kt
@@ -0,0 +1,25 @@
+package oncall.domain
+
+import oncall.domain.model.Holiday
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class HolidayTest {
+
+    @Test
+    fun `검사 값이 휴일인 경우 테스트`(){
+        assertThat(Holiday.checkIsHoliday(1,1))
+        assertThat(Holiday.checkIsHoliday(3,1))
+        assertThat(Holiday.checkIsHoliday(5,5))
+        assertThat(Holiday.checkIsHoliday(6,6))
+        assertThat(Holiday.checkIsHoliday(8,15))
+        assertThat(Holiday.checkIsHoliday(10,3))
+        assertThat(Holiday.checkIsHoliday(10,9))
+        assertThat(Holiday.checkIsHoliday(12,25))
+    }
+
+    @Test
+    fun `검사 값이 휴일이 아닌 경우 테스트`(){
+        assertThat(Holiday.checkIsHoliday(1,2))
+    }
+}
