48237b8c79ac0307a2136713daaa85373ac1d717
jinuemong <jinwoo3661@naver.com>
2023-12-16 16:06:43 +0900

    [Feat]: 출력을 담당하는 OutputView 기능 구현
    (cherry picked from commit 6be5e7d9beeb93d28a3ac6fd72862d211e4f185a)

---
+++ src/main/kotlin/oncall/view/OutputView.kt
@@ -0,0 +1,22 @@
+package oncall.view
+
+import oncall.domain.model.Calendar
+import oncall.domain.model.DayOfWeek
+import oncall.domain.utils.Rule
+
+class OutputView {
+
+    fun dayResult(
+        month: Int,
+        calendar: Calendar,
+        worker: String,
+    ) {
+        val monthMessage = "$month${Rule.MONTH_UNIT}"
+        val dayMessage = "${calendar.day}${Rule.DAY_UNIT}"
+        val dayOfWeekMessage = calendar.dutyDay.getDayOfWeek()
+        val holidayMessage = if (calendar.dayOfWeek.isHoliday()) calendar.dayOfWeek.getInfo() else ""
+
+        println("$monthMessage $dayMessage $dayOfWeekMessage$holidayMessage $worker")
+    }
+
+}
