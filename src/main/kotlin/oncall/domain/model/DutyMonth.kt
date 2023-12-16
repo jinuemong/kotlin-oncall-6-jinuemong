861cd7028abdc2712e73570601f854c828831853
jinuemong <jinwoo3661@naver.com>
2023-12-16 14:34:56 +0900

    [Feat]: 각 달의 최종 일을 관리하는 클래스 기능 구현
    (cherry picked from commit 7003cecd506ee884efb18be848700acddda3592b)

---
+++ src/main/kotlin/oncall/domain/model/DutyMonth.kt
@@ -0,0 +1,29 @@
+package oncall.domain.model
+
+enum class DutyMonth(
+    private val month: Int,
+    private val maxDay: Int
+) {
+
+    JAN(1,31),
+    FEB(2,28),
+    MAR(3,31),
+    APR(4,30),
+    MAY(5,31),
+    JUN(6,30),
+    JUL(7,31),
+    AUG(8,31),
+    SEP(9,30),
+    OCT(10,31),
+    NOV(11,30),
+    DEC(12,31);
+
+
+    companion object{
+        fun getMaxDayFromMonth(month: Int): Int{
+            return entries.find { it.month == month }?.maxDay
+                ?: throw IllegalArgumentException() // 여기에 에러 메시지 추가
+        }
+    }
+
+}
