a4ac2ccf1804d330f4ae3a41c1d147c56498fc0b
jinuemong <jinwoo3661@naver.com>
2023-12-16 16:06:43 +0900

    [Feat]: 출력을 담당하는 OutputView 기능 구현
    (cherry picked from commit 6be5e7d9beeb93d28a3ac6fd72862d211e4f185a)

--- src/main/kotlin/oncall/controller/WorkController.kt
+++ src/main/kotlin/oncall/controller/WorkController.kt
@@ -1,4 +1,33 @@
 package oncall.controller
 
-class WorkController {
+import oncall.domain.DutyCalendar
+import oncall.domain.DutyManager
+import oncall.domain.model.Calendar
+import oncall.view.InputView
+
+class WorkController(
+    private val inputView: InputView,
+) {
+    lateinit var dutyCalendar: DutyCalendar
+    lateinit var dutyManager: DutyManager
+
+    fun userFlow(){
+        makeCalendar()
+    }
+
+    private fun makeCalendar(){
+        val (month,day) = inputView.inputDay()
+        dutyCalendar = DutyCalendar(month.toInt(),day)
+    }
+
+    private fun makeManager(){
+        val weekWorker = inputView.inputWeekDayWorker()
+        val holidayWorker = inputView.inputHolliDayWorker()
+        dutyManager = DutyManager(
+            weekWorker,
+            holidayWorker,
+            dutyCalendar
+        )
+    }
+
 }
