fe17df261460ca31762c8d4a447b2e2584c544cf
jinuemong <jinwoo3661@naver.com>
2023-12-16 16:54:49 +0900

    [Feat]: 컨트롤러에 검증 기능 추가 후 적용, 검증되지 않은 값 입력 시 재 입력 요구
    (cherry picked from commit 337e745b8c36fae83671f282cd0d19906301e799)

--- src/main/kotlin/oncall/controller/WorkController.kt
+++ src/main/kotlin/oncall/controller/WorkController.kt
@@ -2,12 +2,14 @@ package oncall.controller
 
 import oncall.domain.DutyCalendar
 import oncall.domain.DutyManager
+import oncall.domain.DutyVerifier
 import oncall.view.InputView
 import oncall.view.OutputView
 
 class WorkController(
     private val inputView: InputView,
-    private val outputView: OutputView
+    private val outputView: OutputView,
+    private val dutyVerifier: DutyVerifier
 ) {
     private lateinit var dutyCalendar: DutyCalendar
     private lateinit var dutyManager: DutyManager
@@ -19,18 +21,37 @@ class WorkController(
     }
 
     private fun makeCalendar() {
-        val (month, day) = inputView.inputDay()
-        dutyCalendar = DutyCalendar(month.toInt(), day)
+        while (!::dutyCalendar.isInitialized){
+            try {
+                val userInput = inputView.inputDay()
+                dutyVerifier.checkDay(userInput)
+
+                val (month,day) = userInput
+                dutyCalendar = DutyCalendar(month.toInt(), day)
+            } catch (e: IllegalArgumentException){
+                println(e.message)
+            }
+        }
     }
 
     private fun makeManager() {
-        val weekWorker = inputView.inputWeekDayWorker()
-        val holidayWorker = inputView.inputHolliDayWorker()
-        dutyManager = DutyManager(
-            weekWorker,
-            holidayWorker,
-            dutyCalendar
-        )
+        while (!::dutyManager.isInitialized){
+            try {
+                val weekWorker = inputView.inputWeekDayWorker()
+                val holidayWorker = inputView.inputHolliDayWorker()
+                dutyVerifier.checkWorkerDuplicate(weekWorker,holidayWorker)
+                dutyVerifier.checkWorkerComposition(weekWorker.toSet(),holidayWorker.toSet())
+                dutyVerifier.checkWorkerNames(weekWorker)
+
+                dutyManager = DutyManager(
+                    weekWorker,
+                    holidayWorker,
+                    dutyCalendar
+                )
+            } catch (e: IllegalArgumentException){
+                println(e.message)
+            }
+        }
     }
 
     private fun result() {
