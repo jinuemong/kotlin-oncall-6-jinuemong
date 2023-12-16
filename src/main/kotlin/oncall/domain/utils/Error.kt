5ac3900181d7becf028aa857a9003e832503c3c7
jinuemong <jinwoo3661@naver.com>
2023-12-16 17:18:57 +0900

    [Refactor]: 요일을 관리하는 DutyDay 클래스 에러 메시지 추가 후 리팩토링
    (cherry picked from commit 0dad219165d876488882899bba0a3076a0d80967)

--- src/main/kotlin/oncall/domain/utils/Error.kt
+++ src/main/kotlin/oncall/domain/utils/Error.kt
@@ -5,6 +5,6 @@ object Error {
     private const val ERR_MSG = "[ERROR]"
 
     const val ERR_INPUT = "$ERR_MSG 유효하지 않은 입력 값입니다. 다시 입력해 주세요."
-
     const val ERR_DUTY_MONTH = "$ERR_MSG 유효하지 않은 달 입니다."
+    const val ERR_DUTY_DAT_DATE ="$ERR_MSG 유효하지 않은 요일 입니다."
 }
