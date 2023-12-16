748885de9be40af9416df18cb4ea259dc2bb7fbf
jinuemong <jinwoo3661@naver.com>
2023-12-16 15:47:38 +0900

    [Fix]: 교체 알고리즘이 적용되지 않는 버그 수정
    (cherry picked from commit 5b2c07de0138ed5ed631dbce7ff3b27d8ea0554b)

--- src/main/kotlin/oncall/domain/DutyManager.kt
+++ src/main/kotlin/oncall/domain/DutyManager.kt
@@ -13,7 +13,7 @@ class DutyManager(
         allocateWork()
     }
 
-    fun allocateWork() {
+    private fun allocateWork() {
         var weekDayIdx = 0
         var holidayIdx = 0
 
@@ -22,63 +22,65 @@ class DutyManager(
             when (calendar.dayOfWeek) {
                 DayOfWeek.WEEKDAY -> {
                     allocateWeekDay(weekDayIdx)
-                    weekDayIdx+=1
+                    weekDayIdx += 1
                 }
 
-                DayOfWeek.HOLIDAY,DayOfWeek.WEEKEND -> {
+                DayOfWeek.HOLIDAY, DayOfWeek.WEEKEND -> {
                     allocateHoliday(holidayIdx)
-                    holidayIdx+=1
+                    holidayIdx += 1
                 }
 
             }
         }
     }
 
-    private fun allocateWeekDay(weekDayIdx: Int){
-        if (checkDoubleWork(getWeekDayUserName(weekDayIdx))){
+    private fun allocateWeekDay(weekDayIdx: Int) {
+        if (checkDoubleWork()) {
             changeAddUser(getWeekDayUserName(weekDayIdx))
-        }else{
+        } else {
             addUser(getWeekDayUserName(weekDayIdx))
         }
     }
 
-    private fun allocateHoliday(holidayIdx: Int){
-        if (checkDoubleWork(getHolidayUserName(holidayIdx))){
+    private fun allocateHoliday(holidayIdx: Int) {
+        if (checkDoubleWork()) {
             changeAddUser(getHolidayUserName(holidayIdx))
-        }else {
+        } else {
             addUser(getHolidayUserName(holidayIdx))
         }
     }
 
-    private fun getWeekDayUserName(idx:Int): String{
-        return weekDayWorkers[idx%weekDayWorkers.size]
+    private fun getWeekDayUserName(idx: Int): String {
+        return weekDayWorkers[idx % weekDayWorkers.size]
     }
 
-    private fun getHolidayUserName(idx:Int): String {
-        return holidayWorkers[idx%weekDayWorkers.size]
+    private fun getHolidayUserName(idx: Int): String {
+        return holidayWorkers[idx % weekDayWorkers.size]
     }
 
-    private fun changeAddUser(newName: String){
+    private fun changeAddUser(newName: String) {
         val prev = getPrevName()
-        allocationTable[allocationTable.size -1 ] = newName
+        allocationTable[allocationTable.size - 1] = newName
         allocationTable.add(prev)
     }
 
-    private fun addUser(newName: String){
+    private fun addUser(newName: String) {
         allocationTable.add(newName)
     }
 
-    private fun checkDoubleWork(name:String): Boolean {
+    private fun checkDoubleWork(): Boolean {
         if (checkTableSize()) {
-            return getPrevName() == name
+            return getPrevName() == getDoublePrevName()
         }
         return false
     }
 
 
-    private fun checkTableSize(): Boolean = allocationTable.size > 0
+    private fun checkTableSize(): Boolean = allocationTable.size >= 2
 
-    private fun getPrevName(): String = allocationTable.last()
+    private fun getPrevName(): String = allocationTable.last().toString()
+
+    private fun getDoublePrevName(): String = allocationTable[allocationTable.size - 2]
 
 
 }
