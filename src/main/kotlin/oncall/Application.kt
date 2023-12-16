27e8e65bb41f02a78a669348b632cbadfb5007b8
jinuemong <jinwoo3661@naver.com>
2023-12-16 15:29:30 +0900

    [Feat]: 캘린더를 확인하고 근무를 배정하는 DutyManager 클래스 기능 구현
    (cherry picked from commit 64413be0c4406c4814aed0f4d9e1cf43173582cf)

--- src/main/kotlin/oncall/Application.kt
+++ src/main/kotlin/oncall/Application.kt
@@ -1,10 +1,19 @@
 package oncall
 
 import oncall.domain.DutyCalendar
+import oncall.domain.DutyManager
 
 fun main() {
     val calendar = DutyCalendar(5,"월")
     calendar.getDutyCalendar().forEach {
         println(it.day.toString()+","+it.dutyDay+","+it.dayOfWeek)
+    }
+    val dutyMutableList = DutyManager(
+        weekDayWorkers = listOf("준팍","도밥","고니","수아","루루","글로","솔로스타","우코","슬링키","참새","도리"),
+        holidayWorkers = listOf("수아","루루","글로","솔로스타","우코","슬링키","참새","도리","준팍","도밥","고니"),
+        calendar = calendar
+    )
+    dutyMutableList.allocationTable.forEach{
+        println(it)
     }
 }
