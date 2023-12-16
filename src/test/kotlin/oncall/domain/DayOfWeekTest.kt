349573bb857f3f26f81134db09cc177fb93f3a79
jinuemong <jinwoo3661@naver.com>
2023-12-16 17:58:19 +0900

    [Refactor]: 테스트 코드 리팩토링
    (cherry picked from commit 58c6f15325aba6be7e5293a73948aa4e86586ca9)

--- src/test/kotlin/oncall/domain/DayOfWeekTest.kt
+++ src/test/kotlin/oncall/domain/DayOfWeekTest.kt
@@ -10,23 +10,24 @@ import org.junit.jupiter.api.assertThrows
 class DayOfWeekTest {
 
     @Test
-    fun `정확한 요일 입력과 휴일 시 테스트`(){
+    fun `정확한 요일 입력과 휴일 시 테스트`() {
         assertThat(
-            DayOfWeek.getDayOfWeekFromDutyDay(DutyDay.TUESDAY,true)==DayOfWeek.HOLIDAY
+            DayOfWeek.getDayOfWeekFromDutyDay(DutyDay.TUESDAY, true) == DayOfWeek.HOLIDAY
         )
     }
 
     @Test
-    fun `정확한 요일 입력과 휴일이 아닌 경우 테스트`(){
+    fun `정확한 요일 입력과 휴일이 아닌 경우 테스트`() {
         assertThat(
-            DayOfWeek.getDayOfWeekFromDutyDay(DutyDay.TUESDAY,false)==DayOfWeek.WEEKDAY
+            DayOfWeek.getDayOfWeekFromDutyDay(DutyDay.TUESDAY, false) == DayOfWeek.WEEKDAY
         )
     }
 
     @Test
     fun `정확하지 않은 요일 시 테스트`() {
         assertThrows<IllegalArgumentException>(Error.ERR_DUTY_DAT_OF_WEEK) {
-            DayOfWeek.getDayOfWeekFromDutyDay(null,false)
+            DayOfWeek.getDayOfWeekFromDutyDay(null, false)
         }
     }
+
 }
